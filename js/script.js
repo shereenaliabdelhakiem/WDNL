64667fe0f3db3a6243649af6def2f7352ec0903d
shereenaliabdelhakiem <shereenali854@yahoo.com>
2024-08-01 06:24:21 -0700

    d

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ document.querySelector('.navbar-toggler').addEventListener('click', function() {
     
     document.body.classList.toggle('no-scroll');
 });
+document.addEventListener('scroll', function() {
+    const backToTopButton = document.querySelector('.back_to_top');
+    const scrollPosition = window.scrollY;
+    const viewportHeight = window.innerHeight;
+
+    if (scrollPosition > viewportHeight) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
+
+// Optional: Smooth scrolling to top
+document.querySelector('.back_to_top').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
