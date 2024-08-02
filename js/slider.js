c52676a67582d155bbf4ce6e4df8f9b38559b5ad
shereenaliabdelhakiem <shereenali854@yahoo.com>
2024-08-02 15:01:16 -0700

    d

---
+++ js/slider.js
@@ -0,0 +1,36 @@
+$('.partners_slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    autoplay: true,
+    centerMode: true,
+    // centerPadding: '60px',
+    autoplaySpeed:600,
+    speed: 600,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          // centerMode: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          // centerMode: false,
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+    ]
+  });
